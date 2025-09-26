63251fa6ef7002757f4e2a9aa93a699c095a62da
migyny <dianakorzhenevska@yahoo.com>
2025-09-25 21:00:38 -0400

    practice for unit 2

--- practice1.py
+++ practice1.py
@@ -1,3 +1,4 @@
+'''
 print ("hello world")
 
 print("testing connection with github")
@@ -12,3 +13,34 @@ for indx, item in enumerate(tuple_sample):
         result.append(indx)
     
 print(result)
+'''
+
+# unit 2 practice 
+
+x = 2.5
+x= int(x)
+print(x)
+print (type(x))
+print(float('2.5'))
+smallest_value={'a':2,'b':1,'d':4, 'c':10}
+
+print(min(smallest_value.keys())) # Evaluates to 'a'
+#add or change value in the stored key
+smallest_value['e'] = 5
+print('modified dict with added \'e\' element', smallest_value)
+#two ways to print a value (second is safer)
+print('print value with []: ',smallest_value['a'])
+print('print value with get.() method: ',smallest_value.get('f','not found'))
+
+smallest_value.pop('a')
+print('popping using dict.pop(key,default var) \'a\':\n',smallest_value,'\n')
+keys = smallest_value.keys()
+print('print keys using dict.keys():\n',keys,'\n')
+
+values = smallest_value.values()
+print('print values (dict.values()):\n',values,'\n')
+
+items = smallest_value.items()
+print('print all items in dict using dict.items():\n',items,'\n')
+
+print('sorting dictionary using sorted(dict_name):\n',sorted(smallest_value))
